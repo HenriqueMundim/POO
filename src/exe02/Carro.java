5f125f8cdc81c93d43f6017a2fdb4a4c07d9be03
Henrique Matos <henriquemundimmatos@gmail.com>
2024-04-17 10:44:10 -0300

    feat: add ex02 POO solution

---
+++ src/exe02/Carro.java
@@ -0,0 +1,16 @@
+package exe02;
+
+public class Carro {
+    private Pneu p1;
+    private Pneu p2;
+    private Pneu p3;
+    private Pneu p4;
+
+    public void ligar() {
+        System.out.println("Carro ligado");
+    }
+
+    public void desligar() {
+        System.out.println("Carro desligado");
+    }
+}
---
+++ src/exe02/Pneu.java
@@ -0,0 +1,9 @@
+package exe02;
+
+public class Pneu {
+    private int pressao;
+
+    public void rodar() {
+        System.out.println("O pneu está rodando...");
+    }
+}
